4efc2131a42b2432143af192d61eb6c02f28933b
L1P <gast.philipp.lang@suva.ch>
2015-02-11 09:25:02 +0100

    Switched to new jasmine version 2.2.0

--- src/main/java/com/github/searls/jasmine/runner/SpecRunnerHtmlGenerator.java
+++ src/main/java/com/github/searls/jasmine/runner/SpecRunnerHtmlGenerator.java
@@ -1,11 +1,11 @@
 package com.github.searls.jasmine.runner;
 
 public interface SpecRunnerHtmlGenerator {
-	String DEFAULT_SOURCE_ENCODING = "UTF-8";
-	String  JASMINE_JS = "/webjars/jasmine/2.0.0/jasmine.js";
-	String  JASMINE_HTML_JS = "/webjars/jasmine/2.0.0/jasmine-html.js";
+    String DEFAULT_SOURCE_ENCODING = "UTF-8";
+    String  JASMINE_JS = "/webjars/jasmine/2.2.0/jasmine.js";
+    String  JASMINE_HTML_JS = "/webjars/jasmine/2.2.0/jasmine-html.js";
   String  JASMINE_BOOT_JS = "/classpath/lib/boot.js";
-	String  JASMINE_CSS = "/webjars/jasmine/2.0.0/jasmine.css";
+    String  JASMINE_CSS = "/webjars/jasmine/2.2.0/jasmine.css";
 
-	String generate();
+    String generate();
 }
